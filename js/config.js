18c1c72689fa03714a746e25ea614be4973721b0
letstrygg <65213506+letstrygg@users.noreply.github.com>
2026-03-19 02:07:06 -0700

    2

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+        const SUPABASE_URL = 'https://fnwmtytnltmqjaflfwyr.supabase.co';
+        const CONFIG.SUPABASE_ANON_KEY = 'sb_publishable_y12qZF_dSbUPmV_aieiUgA_CibDsxQV';
+};
